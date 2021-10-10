927b0ff98293f85bc90a5531275696b15468a316
DgrinderHZ <hassanzekkouri@gmail.com>
2021-10-10 13:31:20 +0100

    Finish QuickStart Tuto

--- drfqstrt/urls.py
+++ drfqstrt/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('qstart.urls')),
+    path('api-auth/', include(
+        'rest_framework.urls',
+        namespace='rest_framework'
+    )),
 ]
